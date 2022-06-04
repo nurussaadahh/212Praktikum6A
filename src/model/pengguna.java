d2343130e695204dbe441663ba716d0ec0ff69f5
saadah <saadahnurus116@gmail.com>
2022-06-04 22:53:55 +0800

    Membuat Getter and Setter Pengguna

--- src/model/pengguna.java
+++ src/model/pengguna.java
@@ -18,4 +18,44 @@ public class pengguna {
         this.namaLengkap = namaLengkap;
         this.level = level;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNamaLengkap() {
+        return namaLengkap;
+    }
+
+    public void setNamaLengkap(String namaLengkap) {
+        this.namaLengkap = namaLengkap;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
 }
